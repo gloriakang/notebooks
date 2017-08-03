ea34f9ce47bc42105fa58671cd87fc9c7daa6f61
Gloria Kang <gkang@vbi.vt.edu>
2017-08-03 15:38:05 -0400

    add files

---
+++ socnet.R
@@ -0,0 +1,6 @@
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+
+#df_a <- read.csv("../tlc_data/socnet.txt.1.A")
+#head(df_a)
---
+++ tlc_scratch/protting.R
@@ -0,0 +1,5 @@
+library(ggplot2)
+
+intv <- read.csv("aa_0_ai.csv")
+infection_trace <- read.csv("aa_0_infection_trace.csv")
+
